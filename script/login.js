bab0ef1dace712278448b81ef7b5098ce334ec28
raporpe <raporpe@gmail.com>
2020-11-02 22:44:57 +0100

    Added more comments

--- script/login.js
+++ script/login.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    // Add handlers for button clicks
     $("#login-button").click(login);
     $("#switch-register-desktop").click(slide_register);
     $("#switch-login-desktop").click(slide_login);
@@ -7,22 +9,24 @@ $(document).ready(function () {
     $("#delete").click(reset_form);
     $("#submit").click(register);
 
-//    small_login();
-
+    // The bachelor degree selector is hidden by default in the registration form
     $("#bachelor-degree").fadeOut(500);
-    $("#reg_form").on("change", function () {
-        if ($(this).find("input[name=role]:checked").val() == "student") {
-            $("#bachelor-degree").fadeIn(500);
-            $("#comp-eng").attr('required', true);
-        } else {
-            $("#bachelor-degree").fadeOut(500);
-            $("#comp-eng").attr('required', false);
-        }
-    });
 
-    
+    // Call function to update the form when student is selected as role
+    $("#reg_form").on("change", updateForm);
+
 });
 
+// Updates the form to include bachelor degree selection when student is selected in role
+function updateForm() {
+    if ($(this).find("input[name=role]:checked").val() == "student") {
+        $("#bachelor-degree").fadeIn(500);
+        $("#comp-eng").attr('required', true);
+    } else {
+        $("#bachelor-degree").fadeOut(500);
+        $("#comp-eng").attr('required', false);
+    }
+}
 
 // Resets the registration form to defaults
 function reset_form() {
@@ -34,24 +38,27 @@ function login() {
     var username = $("#login_username").val();
     var password = $("#login_password").val();
 
+    // Hide login message in case a previous one was triggered
     $("#login-message").fadeOut(500);
 
+    // Check for empty fields
     if (username == "" || password == "") {
         $("#login-message").text("The password or username are empty");
         $("#login-message").fadeIn(500);
         return;
     }
 
-
+    // Compare the entered password hashed with the already stored password hashed
     var hashedPassword = MD5(password);
     var hashedCookie = getCookie("password");
 
+    // Authentication success
     if (hashedPassword == hashedCookie) {
         setCookie("authenticated", "true");
         window.location.href = "/course.html";
 
     } else {
-        //Do nothing
+        // Authentication fail. Show message.
         $("#login-message").text("The user or password are incorrect");
         $("#login-message").fadeIn(500);
 
@@ -59,9 +66,10 @@ function login() {
 }
 
 function register() {
-    //Fields are prevalidated
+    
+    // Fields are prevalidated and do not need checks
     var register = $("#reg_form").serializeArray();
-    console.log(register);
+
     //Check if email already exists
     var alreadyRegisteredEmail = getCookie("email");
     for (i in register) {
@@ -75,33 +83,31 @@ function register() {
     //Save registration form info in cookies
     var language = "";
     for (i in register) {
-        
+
         if (register[i]["name"] == "password") {
             var password = register[i]["value"];
-        }
-        
-        if (register[i]["name"] == "language") {
+        } else if (register[i]["name"] == "language") {
             language += register[i]["value"] + ",";
+        } else {
+            addCookie(register[i]);
         }
-        
-        addCookie(register[i]);
-        
+
     }
-    
+
     // Store the password hashed
     setCookie("password", MD5(password));
-    
-    // Remove trailing comma and set language
+
+    // Remove trailing comma and set language cookie
     language = language.replace(/,*$/, "");
     setCookie("language", language);
 
+    // Successful registration, go to top and move to login.
     $(".box").scrollTop(0);
     alert("Your registration has been recorded");
     reset_form();
 
-    //Detect if in mobile or desktop version
+    //Move to login. Detect if in mobile or desktop version to execute correct function.
     if ($('#box-header-small').css('display') == 'none') {
-        console.log("Sliding in desktop");
         slide_login();
     } else {
         small_login();
@@ -110,6 +116,8 @@ function register() {
 
 }
 
+
+// Called to make animation sliding to register form in desktop mode 
 function slide_register() {
 
     // Hide the login box
@@ -142,8 +150,9 @@ function slide_register() {
 
 }
 
+// Called to make animation sliding to login form in desktop mode 
 function slide_login() {
-    
+
     // Hide the register box
     $("#box-content-register").css("opacity", "0");
     $("#box-content-register").css("z-index", "-1");
@@ -176,22 +185,27 @@ function slide_login() {
 }
 
 
-
+// Called to make animation sliding to register form in mobile mode 
 function small_register() {
 
+    // Show #box-content-register
     $("#box-content-register").css("opacity", 1);
     $("#box-content-register").css("z-index", "999");
-    
+
+    // Hide #box-content-login
     $("#box-content-login").css("opacity", 0);
     $("#box-content-login").css("z-index", "-1");
 
 }
 
+// Called to make animation sliding to login form in mobile mode 
 function small_login() {
 
+    // Hide #box-content-register
     $("#box-content-register").css("opacity", 0);
     $("#box-content-register").css("z-index", "-1");
-    
+
+    // Show #box-content-login
     $("#box-content-login").css("opacity", 1);
     $("#box-content-login").css("z-index", "999");
 
